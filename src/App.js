ff1ff09203d1239073432517ebcddacd8965e8cb
peebaKU <baramee.254450@gmail.com>
2024-02-24 01:16:45 +0700

    add component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import Post from './Post'
-import Comment from './Comment';
+import Post from './component/Post'
+import Comment from './component/Comment';
+import Child1 from './component/Child1';
+import Child2 from './component/Child2';
+
 function App() {
   const myName = 'Baramee'
   return (
@@ -8,6 +11,8 @@ function App() {
       <Post/>
       <p>ชื่อผมชื่อว่า{ myName }</p>
       <Comment/>
+      <Child1></Child1>
+      <Child2></Child2>
     </div>
   );
 }
